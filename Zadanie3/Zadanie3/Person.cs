34e3c624558e8b87fdf8b2989e245937267f8095
awitek <arkadiuszwk@gmail.com>
2018-09-27 15:46:38 +0200

    1.0

---
+++ Zadanie3/Zadanie3/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3
+{
+    public class Person : IComparable<Person>
+    {
+        private string v1;
+        private int v2;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        internal static void PrintAscending(ICollection<Person> collection)
+        {
+
+
+
+        }
+
+        internal static void PrintOccurrenceOfGivenAge(ICollection<Person> collection, int age)
+        {
+            bool found = false;
+            foreach (Person osoba in collection)
+            {
+                if (osoba.Age == age)
+                {
+                    found = true;
+                    break;
+                }
+            }
+            Console.WriteLine(found);
+        }
+
+        public int CompareTo(Person other)
+        {
+            if (this.Age > other.Age)
+                return -1;
+            else if (this.Age < other.Age)
+                return 1;
+            else return 0;
+
+        }
+    }
+}
---
+++ Zadanie3/Zadanie3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+            persons.Add(new Person("Arek", 28));
+            persons.Add(new Person("John", 18));
+            persons.Add(new Person("Daniel", 50));
+            persons.Add(new Person("Maciej", 10));
+
+
+            Person.PrintOccurrenceOfGivenAge(persons, 18);
+            persons.Sort();
+            foreach (Person p in persons)
+            {
+                if (p.Age >= 18)
+                    Console.WriteLine(p.Age);
+            }
+            Console.ReadKey();
+        }
+    }
+}
